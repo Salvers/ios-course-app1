7cc2bcc348014be45e6bef9003db633f2451a3cb
Mike <mikecalvey@Mikes-MacBook-Pro.local>
2015-11-15 19:46:46 +0000

    added files for app1

--- MEGALOLZZ/ViewController.swift
+++ MEGALOLZZ/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var coollogo: UIImageView!
+    @IBOutlet weak var coolbg: UIImageView!
+    @IBOutlet weak var uncoolbutton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makemelol(sender: AnyObject) {
+        coollogo.hidden = false
+        coolbg.hidden = false
+        uncoolbutton.hidden = true
+    }
 
 }
 
